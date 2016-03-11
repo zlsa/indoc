a62ec0f8cc9ad27778affca56bd80b0cfc8ceb35
Jon Ross <jonross.zlsa@gmail.com>
2016-03-10 16:24:00 -0800

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+rm -rf docs || exit 0;
+mkdir docs;
+
+node ./bin/main.js bin/*.js lib/*.js --name Indoc --readme README.md 
+
+cd docs
+
+git init
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+echo '[credential]' >> .git/config
+echo '  helper = store' >> .git/config
+
+git push --force --quiet https://github.com/zlsa/indoc.git master:gh-pages #> /dev/null 2>&1
